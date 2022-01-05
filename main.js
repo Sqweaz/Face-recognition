a21998357993b36b57e30cc1f9e037b7a30e7d51
Sqweaz <87286501+Sqweaz@users.noreply.github.com>
2022-01-05 10:08:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    widht:350,
+    height:300,
+    imagae_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot(){
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1IvXWam3P/',modelLoaded)
